af8bf67158c01dbd216a44389d523d0bfcdbc43f
Bill Clinton <bclinton@appunto.net>
2024-06-11 09:29:49 -0400

    fix: import web components

--- src/app.js
+++ src/app.js
@@ -3,6 +3,10 @@ import { createActor } from 'xstate';
 import Store from './services/Store.js';
 import AppMachine from './machines/AppMachine.js';
 
+// Link web components
+import HueConfig from './components/HueConfig.js';
+import LightControl from './components/LightControl.js';
+
 // Set up app object
 window.app = {};
 app.store = Store;
