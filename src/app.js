0bfbb416104b0c2c4dd577e5ea67a13041bb8eb5
Bill Clinton <bclinton@appunto.net>
2024-06-23 20:22:32 -0400

    feat: no longer using system object

--- src/app.js
+++ src/app.js
@@ -1,8 +1,4 @@
-// import { createActor } from 'xstate';
-
 import Store from './services/Store.js';
-import System from './services/System.js';
-// import AppMachine from './machines/AppMachine.js';
 
 // Link web components
 import HueConfig from './components/HueConfig.js';
@@ -12,15 +8,12 @@ import LightList from './components/LightList.js';
 // Set up app object
 window.app = {};
 app.store = Store;
-app.system = System;
 
 window.addEventListener('DOMContentLoaded', () => {
   appLoad();
 });
 
 const appLoad = () => {
-  // window.app.system = createActor(AppMachine, {});
-  // app.system.start();
-  // app.system.send({ type: 'initialize' });
-  app.system.start();
+  app.store.initialize();
+  app.store.load();
 };
